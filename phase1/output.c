34cb003f84729c8f76fa3aa0ef80e917863150eb
sagar20896 <vidyasagar20896@gmail.com>
2017-04-11 12:04:17 +0530

    cleaned code

---
+++ phase1/output.c
@@ -0,0 +1,11 @@
+do {
+	
+	int a = 0;
+	int b_a = 1098;
+	float ere2 = 34.33;
+	int __abc = 20+90*78/7;
+	if( a == 0) {
+		break;
+	}
+	printf("Hello world");
+} while (  a >= 0 && b_a == 30 || a > 90);
---
+++ phase2/input.c
@@ -0,0 +1,9 @@
+do
+a=b+c
+c=d+e;
+x=y
+int id=34;
+float fl=90.887;
+char ch='A';
+printf("%d %c %c ",id,fl,ch);
+while a>b)
